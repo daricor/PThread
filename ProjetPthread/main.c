fc89ecdecbec491e59558ab6d9601812e4cecef0
gal30 <galexis30@hotmail.fr>
2017-05-24 21:31:44 +0200

    Ok dest : Manque mutex pour eviter de prendre deux fois la dest

--- ProjetPthread/main.c
+++ ProjetPthread/main.c
@@ -11,9 +11,49 @@ sem_t s_quai;
 #define NOMBRE_QUAIS 4
 #define INIT_CAMIONS 6
 #define NB_COUL 15
+#define NB_DEST 6
 
 
 pthread_t camions;
+pthread_t t_dest;
+
+char* destinations_list[NB_DEST];
+const char* DEST_PROPROSEES[] = {"MONTPELLIER", "MARSEILLE", "LYON", "PARIS", "BORDEAUX", "ARRAS"};
+
+boolean take_dest(unsigned num_cam, unsigned couleur_ale)
+{
+
+    boolean ok = 0;
+    boolean recus = 0;
+    while(!ok)
+    {
+        int index = 0;
+
+        while (index < NB_DEST)
+        {
+            if (destinations_list[index] != "null")
+            {
+                char* tmp_dest = destinations_list[index];
+                destinations_list[index] = "null";
+
+                set_color(couleur_ale, 0);
+                printf("\tN-%i : Destination %s\n", num_cam, tmp_dest);
+                return 1;
+            }
+
+            index++;
+        }
+        if(!recus)
+        {
+            set_color(couleur_ale, 0);
+            printf("\tN-%i : Pas de destinations : En attente\n", num_cam);
+
+            recus = 1;
+        }
+
+    }
+}
+
 
 void* cycleCamion(void* arg)
 {
@@ -43,6 +83,10 @@ void* cycleCamion(void* arg)
 
 
     sem_getvalue(&s_quai,&nombre_quais_libres);
+
+
+    take_dest(selfIdThread, couleur_ale);
+
 
     if (nombre_quais_libres <= 0)
     {
@@ -83,6 +127,42 @@ void* cycleCamion(void* arg)
 
 }
 
+void* destinations(void* arg)
+{
+    srand(time(0));
+
+
+    unsigned max_sleep = 30;
+    unsigned min_rand = 1;
+    unsigned taille_dest_proposees =  sizeof(DEST_PROPROSEES);
+
+
+    while(1)
+    {
+        int index = 0;
+        boolean sortie = 0;
+
+        while (index < NB_DEST && !sortie)
+        {
+            if (destinations_list[index] == "null")
+            {
+                unsigned index_dest = (rand() % (taille_dest_proposees - 0 + 1)) + 0;
+
+                destinations_list[index] = DEST_PROPROSEES[index_dest];
+
+                sortie = 1;
+            }
+
+            index ++;
+        }
+
+        unsigned sleep_time = (rand() % (max_sleep - min_rand + 1)) + min_rand;
+        sleep(sleep_time);
+    }
+
+    pthread_exit ((void*)0);
+
+}
 
 void nouveauCamion()
 {
@@ -115,13 +195,15 @@ int main(int argc, char **argv)
 
     unsigned max_new_camion = 15;
     unsigned min_rand = 0;
-    unsigned nb_camion = 0;
+
+    for(unsigned i = 0; i<NB_DEST; ++i)
+        destinations_list[i] = "null";
 
 
-
+    pthread_create(&t_dest, NULL, destinations, NULL);
     init_cycle(INIT_CAMIONS);
 
-    while (nb_camion <= 50)
+    while (1)
     {
         unsigned time_new_camion = (rand() % (max_new_camion - min_rand + 1)) + min_rand;
         sleep(time_new_camion);
@@ -131,6 +213,7 @@ int main(int argc, char **argv)
 
     int status;
     pthread_join(camions, (void**) &status);
+    pthread_join(t_dest, (void**) &status);
 
     return 0;
 
